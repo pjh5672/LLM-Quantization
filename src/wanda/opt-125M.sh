951ec8d2d767f43149960210b41b3be4d4bbfc30
Jiho Park <pjh5672.dev@gmail.com>
2025-05-05 20:38:02 +0900

    add wanda

---
+++ src/wanda/opt-125M.sh
@@ -0,0 +1,6 @@
+python main.py --model ../../models/opt-125M \
+                --sparsity_ratio 0.5 \
+                --sparsity_type unstructured \
+                --prune_method wanda \
+                --save ../../models/opt-125M-pr50 \
+                --save_model ../../models/opt-125M-pr50
